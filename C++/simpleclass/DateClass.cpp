5eaf6de581cbc9f04ebced39f28130e0470ae115
Bubrak Sher <84930411+Bubrak-Sher@users.noreply.github.com>
2023-10-13 13:27:43 +0500

    Create DateClass.cpp

---
+++ C++/simpleclass/DateClass.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class Date{
+	private:
+		int day;
+		int month;
+		int year;
+		 
+	public:
+		Date(){
+			day = 0;
+			month = 0;
+			year = 0;
+		}
+};
+
+int main()
+{
+	Date date;
+	
+	return 0;
+}
